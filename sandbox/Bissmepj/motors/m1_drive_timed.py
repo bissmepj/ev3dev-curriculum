e9883a47f6a004249cf992f863e58f6edaf54ac8
meyerc1 <meyerc1@rose-hulman.edu>
2018-01-25 09:38:44 -0500

    Changed speak

--- sandbox/Bissmepj/motors/m1_drive_timed.py
+++ sandbox/Bissmepj/motors/m1_drive_timed.py
@@ -97,7 +97,7 @@ def main():
     print("-------------------------------------------")
     print("  Drive using input")
     print("--------------------------------------------")
-    ev3.Sound.speak("Drive using input").wait()
+    ev3.Sound.speak("Gonna fly now").wait()
 
     # Connect two large motors on output ports B and C
     left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
