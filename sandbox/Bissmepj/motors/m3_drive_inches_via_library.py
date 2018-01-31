185aa8c60d4cc1966d2b728ad4045bdac40bba6d
hueync <hueync@rose-hulman.edu>
2018-01-31 09:08:37 -0500

    added

--- sandbox/Bissmepj/motors/m3_drive_inches_via_library.py
+++ sandbox/Bissmepj/motors/m3_drive_inches_via_library.py
@@ -23,6 +23,7 @@ Authors: David Fisher and Nathaniel Huey.
 #  to do a VCS --> Update project...
 # Once the library is implemented each team member should be able to run their version of this code on the robot.
 
+
 # TODO: 4. Formally test your work. When you think you have the problem complete run these tests:
 #   500 dps 24 inches
 #   500 dps -24 inches
