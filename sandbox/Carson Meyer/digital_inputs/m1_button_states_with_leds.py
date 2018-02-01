3e94e2bfff143135de652db53216525d148b0682
meyerc1 <meyerc1@rose-hulman.edu>
2018-02-01 09:17:58 -0500

    M1 for DIGITAL INPUTS committed for TESTING.

--- sandbox/Carson Meyer/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/Carson Meyer/digital_inputs/m1_button_states_with_leds.py
@@ -5,10 +5,10 @@ This module lets you practice using the buttons on the EV3 as states.
 Normally we'll use event callbacks with buttons, but this example uses buttons as states for the purposes of example.
 Much like we did in Bissmepj of the motors unit, later we will show you different (better) ways to use buttons.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Carson Meyer.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
+# DONE: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
 #   You will exit the program by pressing the back button on the EV3 brick (button just below the screen).
 #   The back button is already implemented to exit the program (as you can see in the code below).
 
@@ -53,7 +53,54 @@ def main():
 
     current_color_index = 0
     while True:
-        # TODO: 3. Implement the left, right, and up buttons as follows:
+        count = 0
+        for _ in range(10):
+            if btn.up:
+                print("Up")
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+
+            elif btn.left:
+                print("Left")
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+
+            elif btn.right:
+                print("Right")
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+
+            elif btn.down:
+                count += 1
+                print("Down")
+                if count == 1:
+                    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+                    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+                    break
+
+                if count == 2:
+                    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+                    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+                    break
+
+                if count == 3:
+                    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER)
+                    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER)
+                    break
+
+                if count == 4:
+                    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+                    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+                    count = 0
+                    break
+
+
+            elif btn.backspace:
+                print("Back is pressed")
+            else:
+                print("No button pressed")
+            time.sleep(1.0)
+        # DONE: 3. Implement the left, right, and up buttons as follows:
         #    When the up button is being pressed:
         #      -- print the word "up"
         #      -- turn off all LEDs
