870a646fc823fafa57bf3b947e481ad5c7074e23
hueync <hueync@rose-hulman.edu>
2018-02-08 08:49:44 -0500

    added

--- sandbox/Nate Huey/analog_sensors/color_sensor/m2_follow_a_line.py
+++ sandbox/Nate Huey/analog_sensors/color_sensor/m2_follow_a_line.py
@@ -74,7 +74,7 @@ def follow_the_line(robot, white_level, black_level):
       :type black_level: int
     """
 
-    # TODO: 5. Use the calibrated values for white and black to calculate a light threshold to determine if your robot
+    # DONE: 5. Use the calibrated values for white and black to calculate a light threshold to determine if your robot
     # should drive straight or turn to the right.  You will need to test and refine your code until it works well.
     # Optional extra - For a harder challenge could you drive on the black line and handle left or right turns?
     count_degrees = 0
@@ -85,7 +85,7 @@ def follow_the_line(robot, white_level, black_level):
             count_degrees += 1
             robot.turn_degrees(1,400)
             robot.forward(400,400)
-        if count_degrees == 360:
+        if count_degrees == 400:
             break
 
     robot.stop()
