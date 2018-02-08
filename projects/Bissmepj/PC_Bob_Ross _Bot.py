1751a11093f72647a1f3331b0f3bb75160463b3e
bissmepj <bissmepj@rose-hulman.edu>
2018-02-08 09:23:54 -0500

    Bob Ross

---
+++ projects/Bissmepj/PC_Bob_Ross _Bot.py
@@ -0,0 +1,9 @@
+"""
+The idea behind this project is that the robot acts as a paint brush for a tkinter GUI.
+This includes the ability to lift up or put down the paint brush and to select colors.
+Quit button on Robot, arrow keys to move, u and j to lift arm
+"""
+
+import time
+import ev3dev.ev3 as ev3
+import robot_controller as robo
